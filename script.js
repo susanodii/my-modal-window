7b01e9570a527d3f2db16dda8bdb4285740bf97e
susanodii <odiisusan@gmail.com>
2021-11-11 13:31:43 +0100

    first push of the Modal Window

---
+++ script.js
@@ -0,0 +1,53 @@
+'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+
+const openModel =function(){
+    
+    modal.classList.remove('hidden');
+   overlay.classList.remove('hidden');
+}
+
+const closeModel =  function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+btnsOpenModal[i].addEventListener('click', openModel );
+
+
+
+btnCloseModal.addEventListener('click', closeModel);
+
+overlay.addEventListener('click', closeModel);
+
+document.addEventListener('keydown', function(e){
+    console.log(e.key);
+    if(e.key==="Escape" && !modal.classList.contains('hidden')){
+        {
+            closeModel();
+        }
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
